30e6428e2342b7cde5b939dfece04280966303af
Yann Renard <yannrenard1025@gmail.com>
2024-11-06 19:07:46 +0100

    Refactor code to add FizzBuzz logic in FizzBuzz.java

--- Semaine2/tp02/FizzBuzz.java
+++ Semaine2/tp02/FizzBuzz.java
@@ -1,1 +1,16 @@
 // Exercice 8
+class FizzBuzz extends Program{
+    void algorithm(){
+        print("Entrez un nombre : ");
+        int nombre = readInt();
+        if (nombre % 3 == 0 && nombre % 5 == 0) {
+            println("fizzbuzz");
+        } else if (nombre % 5 == 0) {
+            println("buzz");
+        } else if (nombre % 3 == 0) {
+            println("fizz");
+        } else {
+            println(nombre);
+        }
+    }
+}
