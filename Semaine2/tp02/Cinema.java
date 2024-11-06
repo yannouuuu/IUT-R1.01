cb295bcaf76b0b520e61a45298dd3420d5ef7995
Yann Renard <yannrenard1025@gmail.com>
2024-11-06 19:29:36 +0100

    Refactor code to add ticket pricing logic based on age, 3D option, and subscription status in Cinema.java

--- Semaine2/tp02/Cinema.java
+++ Semaine2/tp02/Cinema.java
@@ -1,1 +1,27 @@
 // Exercice 9
+class Cinema extends Program {
+    void algorithm() {
+        double prixDuBillet = 12;
+        //
+        print("Age du spectateur : ");
+        int ageSpec = readInt();
+        print("Option 3D ? (1 si oui, autre chiffre si non) : ");
+        int option3D = readInt();
+        print("Abonné ? (1 si oui, autre chiffre si non) : ");
+        int estAbonne = readInt();
+        //
+        if (ageSpec < 10) {
+            prixDuBillet /= 2; // Demi-tarif pour les enfants
+        } else if (ageSpec < 16 || ageSpec > 60) {
+            prixDuBillet -= 3; // Réduction de 3€ pour les jeunes et les seniors
+        }
+        if (option3D == 1) {
+            prixDuBillet += 2; // Supplément de 2€ pour la 3D
+        }
+        if (estAbonne == 1) {
+            prixDuBillet *= 0.8; // Réduction de 20% pour les abonnés
+        }
+        //
+        println("Coût du billet : " + prixDuBillet + " euros");
+    }
+}
