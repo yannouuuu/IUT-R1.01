4aa9ade1242942b622a433120cedcfc6dff8f5d9
Yann Renard <yannrenard1025@gmail.com>
2024-11-06 19:00:58 +0100

    Refactor code to add logic for finding the smallest number in PlusPetitNombre.java

--- Semaine2/tp02/PlusPetitNombre.java
+++ Semaine2/tp02/PlusPetitNombre.java
@@ -1,1 +1,13 @@
 // Exercice 7
+class PlusPetitNombre extends Program{
+    void algorithm(){
+        println("Entrez deux nombres :");
+        int premierNb = readInt();
+        int deuxiemeNb = readInt();
+        if (premierNb > deuxiemeNb) {
+            println("Le plus petit nombre est " + deuxiemeNb);
+        } else {
+            println("Le plus petit nombre est " + premierNb);
+        }
+    }
+}
