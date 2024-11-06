edd37d44b3d630e6969a725add19410195027871
Yann Renard <yannrenard1025@gmail.com>
2024-11-06 18:55:18 +0100

    Refactor code to remove leading and trailing spaces in NettoyerChaine.java

--- Semaine2/tp02/NettoyerChaine.java
+++ Semaine2/tp02/NettoyerChaine.java
@@ -1,16 +1,22 @@
 // Exercice 6
 class NettoyerChaine extends Program {
     void algorithm(){
-        String chaine = " Bonjour ";
+        String chaine = " Bonjour  ";
         println("Avant nettoyage :");
         println(">"+chaine+"<");
         int longueurChaine = length(chaine);
-        if(equals(substring(chaine, 0, 1), " ") || equals(substring(chaine, longueurChaine-1, longueurChaine), " ")){
-
-        } else {
-            println("Already perfect");
+        if(equals(substring(chaine, 0, 1), " ") || equals(substring(chaine, longueurChaine-1, longueurChaine), " ")) {
+            int debut = 0;
+            while (debut < longueurChaine && equals(substring(chaine, debut, debut + 1), " ")) {
+                debut++;
+            }
+            int fin = longueurChaine - 1;
+            while (fin >= 0 && equals(substring(chaine, fin, fin + 1), " ")) {
+                fin--;
+            }
+            chaine = substring(chaine, debut, fin + 1);
+            longueurChaine = fin - debut + 1;
         }
-
         println("Après nettoyage :");
         println(">"+chaine+"<");
     }
